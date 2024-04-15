96a342452645a5d72d7f79ab0a8233e34521bedf
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2024-04-15 00:47:46 -0500

    Acabé de implementar todas las funcionalidades
    Me costó bastante entender a qué se refería el
    docente y varias funciones las tuve que buscar
    en internet. Voy a tratar de estudiar de forma
    correcta lo que he construido.
    
    Una reflexión y a seguir...

--- paradigmas/proyectos/agenda/agenda.cpp
+++ paradigmas/proyectos/agenda/agenda.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
+#include <algorithm> 
+#include <cstdlib>   
+#include <ctime>     
 #include <string>
 
-// Definimos nuestra estructura de datos para representar un contacto
-template <typename T>
+template <typename T, typename U, typename V, typename W>
 struct Contacto
 {
     T nombre;
-    T direccion;
-    T numeroDeTelefono;
-    T fechaDeNacimiento;
+    U direccion;
+    V numeroDeTelefono;
+    W fechaDeNacimiento;
 
-    bool operator==(const Contacto<T> &other) const
+    bool operator==(const Contacto<T, U, V, W> &contacto) const
     {
-        return nombre == other.nombre &&
-               direccion == other.direccion &&
-               numeroDeTelefono == other.numeroDeTelefono &&
-               fechaDeNacimiento == other.fechaDeNacimiento;
+        return nombre == contacto.nombre &&
+               direccion == contacto.direccion &&
+               numeroDeTelefono == contacto.numeroDeTelefono &&
+               fechaDeNacimiento == contacto.fechaDeNacimiento;
     }
 };
 
-template <typename T, int maxSize>
+template <typename T, typename U, typename V, typename W, int maxSize>
 class Agenda
 {
 private:
-    Contacto<T> agenda[maxSize];
+    Contacto<T, U, V, W> agenda[maxSize];
     int pos = 0;
 
 public:
-    void agregarContacto(const Contacto<T> &contacto)
+    void agregarContacto(const Contacto<T, U, V, W> &contacto)
     {
         if (pos >= maxSize)
         {
@@ -39,7 +41,7 @@ public:
         std::cout << "Se ha registrado a " << contacto.nombre << " correctamente!" << std::endl;
     }
 
-    void borrarContacto(const Contacto<T> &contacto)
+    void borrarContacto(const Contacto<T, U, V, W> &contacto)
     {
         int index = -1;
 
@@ -67,7 +69,7 @@ public:
         }
     }
 
-    void buscarContacto(const Contacto<T> &contacto)
+    void buscarContacto(const Contacto<T, U, V, W> &contacto)
     {
         bool found = false;
 
@@ -89,21 +91,67 @@ public:
             std::cout << "No se ha encontrado el contacto en la agenda." << std::endl;
         }
     }
+
+    template <typename Compare>
+    void ordenarContactos(Compare comp)
+    {
+        for (int i = 0; i < pos - 1; ++i)
+        {
+            for (int j = 0; j < pos - i - 1; ++j)
+            {
+                if (comp(agenda[j], agenda[j + 1]) == false)
+                {
+                    std::swap(agenda[j], agenda[j + 1]);
+                }
+            }
+        }
+    }
+
+    void desordenarContactos()
+    {
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+        std::random_shuffle(std::begin(agenda), std::begin(agenda) + pos);
+    }
+
+    void buscarContactosPorNombre(const std::string &cadenaBusqueda, int inicio, int fin, std::vector<Contacto<T, U, V, W>> &resultados) const {
+        if (inicio > fin) {
+            return;
+        }
+
+        int medio = inicio + (fin - inicio) / 2;
+
+        // Si el nombre del contacto en la posición media coincide con la cadena de búsqueda, lo agregamos a los resultados
+        if (agenda[medio].nombre == cadenaBusqueda) {
+            resultados.push_back(agenda[medio]);
+        }
+
+        // Buscamos en la mitad izquierda del arreglo si el nombre de la cadena de búsqueda es menor que el nombre del contacto en la posición media
+        buscarContactosPorNombre(cadenaBusqueda, inicio, medio - 1, resultados);
+
+        // Buscamos en la mitad derecha del arreglo si el nombre de la cadena de búsqueda es mayor que el nombre del contacto en la posición media
+        buscarContactosPorNombre(cadenaBusqueda, medio + 1, fin, resultados);
+    }
+
+    // Función pública para buscar contactos por nombre
+    std::vector<Contacto<T, U, V, W>> buscarContactosPorNombre(const std::string &cadenaBusqueda) const {
+        std::vector<Contacto<T, U, V, W>> resultados;
+        buscarContactosPorNombre(cadenaBusqueda, 0, pos - 1, resultados);
+        return resultados;
+    }
+
+    void mostrarContactos() const
+    {
+        for (int i = 0; i < pos; ++i)
+        {
+            std::cout << "Nombre: " << agenda[i].nombre << ", Dirección: " << agenda[i].direccion
+                      << ", Teléfono: " << agenda[i].numeroDeTelefono << ", Fecha de Nacimiento: " << agenda[i].fechaDeNacimiento << std::endl;
+        }
+    }
+
+
 };
 
-int main(void)
-{
-    const int maxSize = 5;
-    Agenda<std::string, maxSize> miAgenda;
+int main() {
 
-    Contacto<std::string> contacto1 = {"Juan", "Calle A", "123456", "01/01/1990"};
-    Contacto<std::string> contacto2 = {"María", "Calle B", "654321", "02/02/1995"};
-    Contacto<std::string> contacto3 = {"Pedro", "Calle C", "987654", "03/03/1985"};
-
-    miAgenda.agregarContacto(contacto1);
-    miAgenda.agregarContacto(contacto2);
-    miAgenda.agregarContacto(contacto3);
-
-    Contacto<std::string> contactoNoHabido = {"Lolo", "asd", "Asd", "Asd"};
-    miAgenda.buscarContacto(contactoNoHabido);
 }
